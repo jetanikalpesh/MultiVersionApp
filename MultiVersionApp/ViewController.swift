f6557b21c6a0cae415e8b6f7e8dc4fe0e1cde1a3
KalpeshJetani <kalpeshjetani@openxcell.info>
2017-04-03 19:33:28 +0530

    Initial Commit

---
+++ MultiVersionApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiVersionApp
+//
+//  Created by Kalpesh on 03/04/17.
+//  Copyright © 2017 Organization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
